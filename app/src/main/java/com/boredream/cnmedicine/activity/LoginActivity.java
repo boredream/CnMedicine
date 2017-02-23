22eee4343ceb38cd0055c76b0acffb89bc3e94d4
lichunyang <lichunyang@qbao.com>
2017-02-23 18:14:24 +0800

    login

--- app/src/main/java/com/boredream/cnmedicine/activity/LoginActivity.java
+++ app/src/main/java/com/boredream/cnmedicine/activity/LoginActivity.java
@@ -10,6 +10,6 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
     }
 }
